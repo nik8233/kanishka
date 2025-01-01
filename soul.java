7a7276e0316f7073e098ec680184b13458b1a5f2
nik8233 <kanik8233@gmail.com>
2025-01-01 16:44:39 +0530

    added ra babu

--- soul.java
+++ soul.java
@@ -1,12 +1,18 @@
+import java.util.Scanner;
 class soul{
     public static void main(String[] args){
-        int num1=2;
-        int num2=2;
-        if(num1==num2){
-            System.out.print("it is equal");
+        Scanner sc=new Scanner(System.in);
+        int m1=sc.nextInt();
+        int m2=sc.nextInt();
+        int m3=sc.nextInt();
+        int m4=sc.nextInt();
+        int m5=sc.nextInt();
+        int m6=m1+m2+m3+m4+m5;
+        if(m6/5>=35){
+            System.out.println("need an extra class");
         }
         else{
-            System.out.println("it is not equal");
+            System.out.println("you are good to go");
         }
     }
 }
